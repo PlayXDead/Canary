c5f200780247139dabaa31cc71e65d4a65bf7e03
Tim <timothy.harris0187@gmail.com>
2025-08-25 21:41:35 -0400

    Canary App

--- flake.nix
+++ flake.nix
@@ -1,31 +1,43 @@
 {
-description = "Flutter 3.13.x";
-inputs = {
-  nixpkgs.url = "github:NixOS/nixpkgs/25.05";
-  flake-utils.url = "github:numtide/flake-utils";
-};
-outputs = { self, nixpkgs, flake-utils }:
-  flake-utils.lib.eachDefaultSystem (system:
-    let
+  description = "Flutter development environment";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # Specify NixOS 25.05
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
         config = {
-          android_sdk.accept_license = true;
-          allowUnfree = true;
+          allowUnfree = true; # Required for Android SDK components
+          android_sdk = {
+            accept_license = true;
+          };
         };
       };
-      androidSdk = pkgs.androidenv.androidPkgs.androidsdk;
-    in
-    {
-      devShell =
-        with pkgs; mkShell rec {
-          ANDROID_SDK_ROOT = "${androidSdk}/libexec/android-sdk";
-          buildInputs = [
-            flutter
-            androidSdk
-            jdk17
-          ];
-        };
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "flutter-dev-shell";
+
+        packages = with pkgs; [
+          flutter
+          jdk # Required for Android development
+          android-tools.adb # For ADB functionality
+          android-tools.fastboot # For fastboot functionality
+          # Add specific Android SDK packages if needed, e.g.,
+          # androidenv.androidPkgs_9_0.androidsdk
+          # You might need to specify specific build-tools versions
+          # like android-sdk.build-tools-34-0-0
+        ];
+
+        # Environment variables for Android SDK
+        shellHook = ''
+          export ANDROID_HOME=${pkgs.android-sdk}/share/android-sdk
+          export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
+          export JAVA_HOME=${pkgs.jdk}
+        '';
+      };
     });
 }
-
