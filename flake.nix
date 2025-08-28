f9537efb5401fff22431f49a285530d9a9f0de83
tim <timothy.harris0187@gmail.com>
2025-08-28 18:32:53 -0400

    Canary App

--- flake.nix
+++ flake.nix
@@ -2,42 +2,46 @@
   description = "Flutter development environment";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05"; # Specify NixOS 25.05
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
     flake-utils.url = "github:numtide/flake-utils";
+    android.url = "github:tadfisher/android-nixpkgs";
   };
 
-  outputs = { self, nixpkgs, flake-utils }:
+  outputs = { self, nixpkgs, flake-utils, android, }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {
         inherit system;
         config = {
-          allowUnfree = true; # Required for Android SDK components
-          android_sdk = {
-            accept_license = true;
-          };
+          allowUnfree = true;
+          android_sdk.accept_license = true;
         };
       };
+
+      # The 'sdk' function is available directly on the `android` output
+      # when the flake is evaluated for the current system.
+      myAndroidSdk = android.sdk.${system} (sdkPkgs: with sdkPkgs; [
+        platforms-android-34
+        build-tools-34-0-0
+        platform-tools
+        cmdline-tools-latest
+      ]);
+
     in {
       devShells.default = pkgs.mkShell {
         name = "flutter-dev-shell";
 
         packages = with pkgs; [
           flutter
-          jdk # Required for Android development
-          android-tools.adb # For ADB functionality
-          android-tools.fastboot # For fastboot functionality
-          # Add specific Android SDK packages if needed, e.g.,
-          # androidenv.androidPkgs_9_0.androidsdk
-          # You might need to specify specific build-tools versions
-          # like android-sdk.build-tools-34-0-0
+          jdk
+          myAndroidSdk
         ];
 
-        # Environment variables for Android SDK
         shellHook = ''
-          export ANDROID_HOME=${pkgs.android-sdk}/share/android-sdk
-          export PATH=$PATH:$ANDROID_HOME/cmdline-tools/latest/bin:$ANDROID_HOME/platform-tools
+          export ANDROID_HOME=${myAndroidSdk}/share/android-sdk
+          export ANDROID_SDK_ROOT=${myAndroidSdk}share/android-sdk
           export JAVA_HOME=${pkgs.jdk}
         '';
       };
     });
 }
+
